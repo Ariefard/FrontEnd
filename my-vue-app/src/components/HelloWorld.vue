e4c8ddc8c85b383886b1fe3d4f39b88f4b40a98a
Ariefard <ariefardi26@gmail.com>
2021-05-30 15:21:13 +0700

    Project Pertama

---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>Selamat Datang {{ myText }}</div>
+    <ul>
+      <li v-for="item in todos">{{ item.desc}}</li>
+      
+    </ul>
+    <input v-model="myText"/>
+    <button @click="tambah">Add</button>
+  </div>
+</template>
+
+
+
+<script>
+export default{
+  data : function(){
+    return{
+      todos : [
+      {desc: 'Makan Durian' },
+      {desc: 'Makan Pisang'},
+      {desc: 'Coding JS'}
+      ],
+      myText: ''
+    }
+  },
+  methods:{
+    tambah : function(){
+      this.todos.push({desc: this.myText})
+    }
+
+  }
+}
+</script>
+
